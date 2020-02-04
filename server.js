4ec9f4eb25a9fc03d72dbb0116a4e3575c396954
dean.h.park <dean@decompany.io>
2020-02-04 15:33:38 +0900

    - server.js 수정

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-import {APP_CONFIG} from "./app.config";
-
 require('dotenv').config();
 
 const express = require('express');
 const next = require('next');
+const {APP_CONFIG} = require('./app.config');
 
 const __ENV__ = (process.env.NODE_ENV && process.env.NODE_ENV.trim().toLowerCase() === 'production') ? 'production' : 'development';
 const __ENV_SUB__ = (process.env.NODE_ENV_SUB && process.env.NODE_ENV_SUB.trim().toLowerCase() === 'production') ? 'production' : 'development';
