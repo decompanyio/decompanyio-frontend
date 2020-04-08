a9c866a4df94ef8de5cd120c1c05f4e4d87b91cc
mac <dean@decompany.io>
2020-04-08 16:16:13 +0900

    - add production auth URL

--- app.config.ts
+++ app.config.ts
@@ -55,7 +55,7 @@ export const APP_CONFIG = {
       viewer: 'https://viewer.polarishare.com/',
       embed: 'https://embed.polarishare.com/',
       graphql: 'https://api.polarishare.com/graphql/',
-      auth: 'https://auth.share.decompany.io'
+      auth: 'https://auth.polarishare.com/'
     }
   }
 }
