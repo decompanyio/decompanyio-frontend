8483019f0fb6849718dbd8f132d279f059a1a9bc
mac <dean@decompany.io>
2020-06-22 11:27:06 +0900

    - test manifest.json

--- next.config.js
+++ next.config.js
@@ -60,34 +60,7 @@ module.exports = withPlugins(
     },
     withOffline: {
       dontAutoRegisterSw: false,
-      generateInDevMode: true,
-      workboxOpts: {
-        swDest: process.env.NEXT_EXPORT
-          ? 'service-worker.js'
-          : 'static/service-worker.js',
-        runtimeCaching: [
-          {
-            urlPattern: /^https?.*/,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'offlineCache',
-              expiration: {
-                maxEntries: 200
-              }
-            }
-          }
-        ]
-      },
-      experimental: {
-        async rewrites() {
-          return [
-            {
-              source: '/service-worker.js',
-              destination: '/_next/static/service-worker.js'
-            }
-          ]
-        }
-      }
+      generateInDevMode: true
     }
   }
 )
