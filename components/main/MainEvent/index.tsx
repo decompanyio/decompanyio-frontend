c160b2b73da3c95f98619722d6c30137ea793355
mac <dean@decompany.io>
2020-09-16 14:34:20 +0900

    test wallet

--- components/main/MainEvent/index.tsx
+++ components/main/MainEvent/index.tsx
@@ -6,16 +6,16 @@ export default function MainEvent(): ReactElement {
   return (
     <div className={styles.mes_wrapper}>
       <div className={styles.mes_inner}>
-        <p className={styles.mes_title_small}>Beta Season</p>
-        <p className={styles.mes_title_big}>Big event</p>
+        {/*<p className={styles.mes_title_small}>Beta Season</p>*/}
+        <p className={styles.mes_title_big}>Beta Season</p>
         <p className={styles.mes_content_a}>{psString('main-event-1')}</p>
-        <p className={styles.mes_content_a}>
+        {/*<p className={styles.mes_content_a}>
           {psString('main-event-2')}
           <br />
           <span className={styles.mes_point}>{psString('main-event-3')}</span>
           {psString('main-event-4')}
         </p>
-        <p className={styles.mes_content_b}>{psString('main-event-5')}</p>
+        <p className={styles.mes_content_b}>{psString('main-event-5')}</p>*/}
       </div>
     </div>
   )
