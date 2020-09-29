fff6029977b2f0703b59e4bf362b98deb1300a67
mac <dean@decompany.io>
2020-09-29 13:49:06 +0900

    - add wallet history tab on profile page

--- service/model/WalletHistoryData.ts
+++ service/model/WalletHistoryData.ts
@@ -17,10 +17,10 @@ export default class WalletHistoryData {
     this.type = data && data.type ? data.type : ''
     this.from = data && data.from ? data.from : ''
     this.to = data && data.to ? data.to : ''
-    this.factor = data && data.factor ? data.factor : ''
-    this.value = data && Number(data.value) ? data.value : ''
+    this.factor = data && data.factor ? data.factor : -1
+    this.value = data && data.value ? Number(data.value) : 0
     this.transactionHash =
       data && data.transactionHash ? data.transactionHash : ''
-    this.created = data && data.created ? data.created : ''
+    this.created = data && data.created ? data.created : 0
   }
 }
